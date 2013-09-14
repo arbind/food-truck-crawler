9bc2f96b0d1f8509ba1c60a58c08bbb54ac5b571
Arbind Thakur and Rob Pak <dev+arbind+rpak@carbonfive.com>
2013-09-14 15:33:01 -0700

    batch states with huge numbers tweeters

--- generate_tweeters_html.rb
+++ generate_tweeters_html.rb
@@ -3,8 +3,15 @@ require "active_support/core_ext"
 HTML_DIR = './tweeters-2013-09-15'
 json_crafts = IO.read './crafts-2013-09-15.json'
 
-def html_file(state)
-  "#{HTML_DIR}/#{state}.html"
+def html_file_name(state, idx)
+  name = state
+  name = "#{name}-#{idx}" if idx > 0
+  "#{name}.html"
+end
+
+
+def html_file(state, idx=0)
+  "#{HTML_DIR}/#{html_file_name(state, idx)}"
 end
 
 crafts = JSON.parse json_crafts
@@ -37,6 +44,22 @@ end
 places = tweeters.map {|tweeter| tweeter[:place]}
 places = places.uniq
 
+states = tweeters.map {|tweeter| tweeter[:state]}
+states = states.uniq
+
+states.sort! do |a,b|
+  tweeters_in_states[b].count <=> tweeters_in_states[a].count
+end
+
+tweeters_in_states.each do |state, tweeters|
+  if tweeters.count.eql? 0  # prune empty states
+    states.delete(state)
+    tweeters_in_states.delete(state)
+  else # batches of 50
+    tweeters_in_states[state] = tweeters.each_slice(50).to_a
+  end
+end
+
 File.open(html_file('1-places'), 'w') do |file_places|
   file_places.write "<html><body>\n<h2>#{places.count} Places</h2>\n"
   places.each do |place|
@@ -45,63 +68,47 @@ File.open(html_file('1-places'), 'w') do |file_places|
   file_places.write "</body></html>\n"
 end
 
-states = tweeters.map {|tweeter| tweeter[:state]}
-states = states.uniq
-
-states.sort! do |a,b|
-  tweeters_in_states[b].count <=> tweeters_in_states[a].count
-end
-
-tweeters_in_states.each do |state, tweeters| # prune empty states
-  if tweeters.count.eql? 0
-    states.delete(state)
-    tweeters_in_states.delete(state)
-  end
-end
-
 File.open(html_file('0-states'), 'w') do |file_states|
   file_states.write "<html><body>\n<h2>#{states.count} States</h2>\n"
   states.each do |state|
-    count = tweeters_in_states[state].count
-    file_states.write "<div><a href='#{state}.html'>#{state} (#{count})</a></div>\n"
+    tweeters_in_states[state].each_with_index do |batch, idx|
+      count = batch.count
+      file_states.write "<div><a href='#{html_file_name(state, idx)}'>#{state}-#{idx+1} (#{count})</a></div>\n"
+    end
   end
   file_states.write "</body></html>\n"
 end
 
 def follow_button_html(screen_name)
-  "<iframe style='min-width:300px; height:20px;' allowtransparency='true' frameborder='0' scrolling='no' src='http://platform.twitter.com/widgets/follow_button.html?screen_name=#{screen_name}&show_count=true&show_screen_name=true'></iframe>"
+  "<iframe style='min-width:300px; height:20px;' allowtransparency='true' frameborder='0' scrolling='no' src='http://platform.twitter.com/widgets/follow_button.html?screen_name=#{screen_name.squish}&show_count=true&show_screen_name=true'></iframe>"
 end
 
 states.each do |state|
-  state_tweeters = tweeters_in_states[state]
-  File.open(html_file(state), 'w') do |file_state|
-    file_state.write "<html><body>\n<h1>#{state}</h1>\n"
-    next_place = state_tweeters.first[:place]
-    file_state.write "<h2>#{next_place}</h2>\n"
-    state_tweeters.each do |tweeter|
-      if tweeter[:href]
-        place = tweeter[:place]
-        unless place.eql? next_place
-          file_state.write "<h2>#{place}</h2>\n"
-          next_place = place
+  batches = tweeters_in_states[state]
+  batches.each_with_index do |state_tweeters, idx|
+    File.open(html_file(state, idx), 'w') do |file_state|
+      file_state.write "<html><body>\n<h1>#{state}</h1>\n"
+      next_place = state_tweeters.first[:place]
+      file_state.write "<h2>#{next_place}</h2>\n"
+      state_tweeters.each_with_index do |tweeter, count|
+        if tweeter[:href]
+          place = tweeter[:place]
+          unless place.eql? next_place
+            file_state.write "<h2>#{place}</h2>\n"
+            next_place = place
+          end
+          screen_name = tweeter[:href].to_s.split('/').last
+          twitter_link = "<A href='#{tweeter[:href]}' target='t'>#{screen_name}</A>"
+          line = "<div style='height:24px;line-height:24px; font-size: 16px'>\n"
+          line = "#{line}  #{idx+1}.#{count+1}. [#{tweeter[:state]}] [#{tweeter[:city]}] \n"
+          line = "#{line}  #{follow_button_html(screen_name)}\n"
+          line = "#{line}  - #{twitter_link}\n"
+          line = "#{line}</div>\n"
+          file_state.write line
         end
-        screen_name = tweeter[:href].to_s.split('/').last
-        twitter_link = "<A href='#{tweeter[:href]}' target='t'>#{screen_name}</A>"
-        line = "<div style='height:24px;line-height:24px; font-size: 16px'>\n"
-        line = "#{line}  [#{tweeter[:state]}] [#{tweeter[:city]}] \n"
-        line = "#{line}  #{follow_button_html(screen_name)}\n"
-        line = "#{line}  - #{twitter_link}\n"
-        line = "#{line}</div>\n"
-        file_state.write line
-      else # no twitter account
-        line = "<div style='height:24px;line-height:24px; font-size: 16px'>\n"
-        line = "#{line}  [#{tweeter[:state]}] [#{tweeter[:city]}] \n"
-        line = "#{line}  - #{tweeter[:name]}\n"
-        line = "#{line}</div>\n"
-        file_state.write line
-      end
 
+      end
+      file_state.write "</body></html>\n"
     end
-    file_state.write "</body></html>\n"
   end
 end
